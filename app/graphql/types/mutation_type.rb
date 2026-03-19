958985cc8ec1fca40c7d49beb80e7c2a632db33c
Saksham Kaundal <kaundalsaksham@gmail.com>
2026-03-19 20:14:02 +0530

    implemented create user

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser, description: "Create a new user."
     field :create_post, mutation: Mutations::CreatePost, description: "Create a new post."
 
     # TODO: remove me
